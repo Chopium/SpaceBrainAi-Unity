397723d156c5b5272124c7f228ca84fb81e50c7a
Colter Wehmeier <wehmeie2@illinois.edu>
2020-01-24 23:18:29 -0600

    Working Commit

---
+++ SpaceBrainAI/Assets/Scripts/Unity Extensions/disableIfEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableIfEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if UNITY_EDITOR || UNITY_STANDALONE
+        this.gameObject.SetActive(false);
+        #endif
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+
+}
